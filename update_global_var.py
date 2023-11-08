aa2690cc8962e9df8183fdec89a0570e8fe1d28f
rakeshroshan0398 <47413149+rakeshroshan0398@users.noreply.github.com>
2023-11-08 12:06:41 +0530

    Update update_global_var.py

--- update_global_var.py
+++ update_global_var.py
@@ -4,19 +4,19 @@ import streamlit as st
 # update_global.clear()
 
 def update_global():
-  if global_vars.global_var is None:
-    global_vars.global_var = 1000
-    global_vars.refresh = True 
-  else:
-    delta = global_vars.global_var
-    global_vars.global_var = delta - 1
-    global_vars.refresh = True
-
-  if global_vars.refresh:
-      st.write(global_vars.global_var)
-      global_vars.refresh = False
-  # if global_vars.global_var is not None:
-  #   global_vars.global_var = global_vars.global_var - 1;
-  # else:
+  # if global_vars.global_var is None:
   #   global_vars.global_var = 1000
+  #   global_vars.refresh = True 
+  # else:
+  #   delta = global_vars.global_var
+  #   global_vars.global_var = delta - 1
+  #   global_vars.refresh = True
+
+  # if global_vars.refresh:
+  #     st.write(global_vars.global_var)
+  #     global_vars.refresh = False
+  if global_vars.global_var is not None:
+    global_vars.global_var = global_vars.global_var - 1;
+  else:
+    global_vars.global_var = 1000
 
