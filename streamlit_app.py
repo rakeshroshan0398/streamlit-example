0fd873174b3b75ad8d16dc254e5ac768b2052592
rakeshroshan0398 <47413149+rakeshroshan0398@users.noreply.github.com>
2023-11-08 12:57:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ if 'key' not in st.session_state:
 global_vars.global_var = global_vars.global_var
 # First time default call for initialization 2
 # st.write('Count = ', global_vars.global_var)
-st.write(f"key = {st.session_state['key']} and count = {global_vars.global_var}")
+st.write(f"key = {st.session_state['key']['count']} and count = {global_vars.global_var}")
 
 if st.button('Update Count'):
   ugv.update_global()
