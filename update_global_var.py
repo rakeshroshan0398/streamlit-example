8f422f52b5ee9b5516a6f5901c6f7f86420c22a2
rakeshroshan0398 <47413149+rakeshroshan0398@users.noreply.github.com>
2023-11-08 12:01:17 +0530

    Update update_global_var.py

--- update_global_var.py
+++ update_global_var.py
@@ -3,20 +3,20 @@ import global_vars
 
 @st.cache_resource
 def update_global():
-  if global_vars.global_var is None:
-    global_vars.global_var = 1000
-    global_vars.refresh = False 
-  else:
-    delta = global_vars.global_var
-    global_vars.global_var = delta - 1
-    global_vars.refresh = True
-
-  if global_vars.refresh:
-      st.write(global_vars.global_var)
-      global_vars.refresh = False
-  # if global_vars.global_var is not None:
-  #   global_vars.global_var = global_vars.global_var - 1;
-  # else:
+  # if global_vars.global_var is None:
   #   global_vars.global_var = 1000
-  # update_global.clear()
+  #   global_vars.refresh = False 
+  # else:
+  #   delta = global_vars.global_var
+  #   global_vars.global_var = delta - 1
+  #   global_vars.refresh = True
+
+  # if global_vars.refresh:
+  #     st.write(global_vars.global_var)
+  #     global_vars.refresh = False
+  if global_vars.global_var is not None:
+    global_vars.global_var = global_vars.global_var - 1;
+  else:
+    global_vars.global_var = 1000
+  update_global.clear()
 
