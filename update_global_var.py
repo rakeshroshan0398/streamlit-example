cc1c3ef0c1f94b14f72f00047ef99b0b2eeea6f5
rakeshroshan0398 <47413149+rakeshroshan0398@users.noreply.github.com>
2023-11-08 12:02:29 +0530

    Update update_global_var.py

--- update_global_var.py
+++ update_global_var.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import global_vars
 
-@st.cache_resource
+# @st.cache_resource
 def update_global():
   # if global_vars.global_var is None:
   #   global_vars.global_var = 1000
@@ -18,5 +18,5 @@ def update_global():
     global_vars.global_var = global_vars.global_var - 1;
   else:
     global_vars.global_var = 1000
-  update_global.clear()
+  # update_global.clear()
 
