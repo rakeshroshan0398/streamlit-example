329fde79eb325204d8ead6b00bbfcd3510831fee
rakeshroshan0398 <47413149+rakeshroshan0398@users.noreply.github.com>
2023-11-08 11:20:08 +0530

    Update update_global_var.py

--- update_global_var.py
+++ update_global_var.py
@@ -1,8 +1,9 @@
 import global_vars
 
 def update_global():
+  @st.cache_resource.clear()
   if global_vars.global_var is not None:
     global_vars.global_var = global_vars.global_var - 1;
   else:
     global_vars.global_var = 1000
-  st.cache_resource.clear()
+  
